74e1569464a7f0c9af742228b13165d8f973732c
marcelo mrad <Dti.Marcelo.Mrad@segurossura.com.br>
2025-05-04 17:28:14 -0300

    6. Classes TestLoader e TestRunner

---
+++ test/test_loader_test.py
@@ -0,0 +1,50 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.core.test_case import TestCase
+from src.core.test_loader import TestLoader
+from src.core.test_suite import TestSuite
+from src.core.test_runner import TestRunner
+from test.test_case_test import TestStub, TestSpy
+
+
+class TestLoaderTest(TestCase):
+
+    def test_create_suite(self):
+        loader = TestLoader()
+        suite = loader.make_suite(TestStub)
+        assert len(suite.tests) == 3
+
+    def test_create_suite_of_suites(self):
+        loader = TestLoader()
+        stub_suite = loader.make_suite(TestStub)
+        spy_suite = loader.make_suite(TestSpy)
+
+        suite = TestSuite()
+        suite.add_test(stub_suite)
+        suite.add_test(spy_suite)
+
+        assert len(suite.tests) == 2
+
+    def test_get_multiple_test_case_names(self):
+        loader = TestLoader()
+        names = loader.get_test_case_names(TestStub)
+        assert sorted(names) == ['test_error', 'test_failure', 'test_success']
+
+    def test_get_no_test_case_names(self):
+        class Test(TestCase):
+            def foobar(self):
+                pass
+
+        loader = TestLoader()
+        names = loader.get_test_case_names(Test)
+        assert names == []
+
+
+if __name__ == "__main__":
+    loader = TestLoader()
+    suite = loader.make_suite(TestLoaderTest)
+
+    runner = TestRunner()
+    runner.run(suite)
